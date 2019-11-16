1b6059f3225251af6f17e174d12926029cb4e805
Michael Cervera <michaelcervera13@gmail.com>
2019-11-16 13:00:53 -0500

    Detail

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -4,57 +4,7 @@ function Footer() {
   return (
     <div className="App">
       <footer>
-        <div class="container">
-          <div class="row">
-            <div class="col-sm">
-            </div>
-            <div class="col-sm">
-              <span class="titulo-pie-pagina"> CONTÁCTANOS</span>
-              <ul class="links">
-                <li>
-                  <h1>
-                     Base de datos de Pueblos Indígenas u
-                  </h1>
-                </li>
-                <li>
-                  <h1>
-                    Tel. +51 (01) 618-9393 anexos 2720 y 2745
-                  </h1>
-                </li>
-                <li>
-                  <h1>
-                    E-mail: renitli@cultura.gob.pe
-                  </h1>
-                </li>
-                <li>
-                  <h1>
-                   bdpi.cultura.gob.pe
-                  </h1>
-                </li>
-              </ul>
-            </div>
-            <div class="col-sm">
-              <span class="titulo-pie-pagina"> ENLACES EXTERNOS</span>
-              <ul class="links">
-                <li><a href="">Centro de recursos interculturales</a></li>
-                <li><a href="">Consulta previa</a></li>
-                <li><a href="">Alerta contra el racismo</a></li>
-                <li><a href="">Mapa sonoro estadístico de lenguas indígenas u originarias</a></li>
-                <li><a href="">Geocultura</a></li>
-              </ul>
-            </div>
-          </div>
-        </div>
-        <div class="row">
-          <div class="col-sm">
-          </div>
-          <div class="col-sm">
-            <p>Sede central Av. Javier Prado Este 2465</p>
-            <p>San Borja, Lima 41 Perú</p>
-            <p>Central Telefònica: 511-6189393</p>
-          </div>
-          <div class="col-sm"></div>
-        </div>
+        footer
       </footer>
     </div>
   );
--- src/components/ProcesoTesting.js
+++ src/components/ProcesoTesting.js
@@ -2,8 +2,42 @@ import React from 'react';
 
 function ProcesoTesting() {
   return (
-    <div className="Proceso">
-       <h2>testnig</h2>
+    <div className="App">
+      <div className="container mt-4 mb-4">
+        <div className="row">
+          <div  className="col-md-12">
+             <span className="title">Concepeto de Calidad</span>
+              <span className="texto"> <br/>
+                Los probadores de software (también conocidos como testers) planifican
+                y llevan a cabo pruebas de software de los ordenadores para comprobar si funcionan correctamente.
+                Identifican el riesgo de sufrir errores de un software, detectan errores y los comunican. Evalúan
+                el funcionamiento general del software y sugieren formas de mejorarlo.
+              </span><br/><br/>
+              <span className="title"> Características del proceso de calidad total</span>
+              <span className="texto">
+                <ul>
+                  <li> Un control de los errores que se van cometiendo para edificar un sistema que los evite antes de que sucedan.</li>
+                  <li> Consiste en el apoyo total de la dirección de la empresa hacia una dinámica de mejora en la calidad y productividad.</li>
+                  <li> Se trata de medir la calidad con instrumentos o métodos adecuados y conseguir que cada una de las personas de la empresa se sienta comprometida personalmente en una nueva forma de trabajar hacia la fiabilidad.</li>
+                  <li> Asegurar en cada fase del proceso productivo que las acciones llevadas a cabo y las características incorporadas en el producto van a contribuir a un resultado final que esté conforme con las necesidades del cliente.</li>
+                  <li> Establecer un lenguaje y unas comunicaciones tales entre personas y empresas que permita transmitir los conceptos sin que existan malentendidos.</li>
+                  <li> Optimización continua del proceso de forma que se vaya reduciendo la variabilidad de los parámetros que intervienen.</li>
+                  <li> Tolerancia cada vez inferior en cuanto a las variaciones del nivel de calidad final de la imagen de los productos.</li>
+                  <li> Un seguimiento estadístico de los resultados obtenidos, a ser posible por Departamento, para valorar el éxito del progreso.</li>
+                  <li> Cumplir con los plazos de entrega convenidos con el cliente.</li>
+                  <li> Disponer de una atmósfera de cordialidad y trato satisfactorio con proveedores y clientes.</li>
+                  <li> Conseguir una mejora creciente de los resultados económicos.</li>
+                  <li> Aumentar la propia capacidad de producción reduciendo progresivamente las ineficacias en los tiempos muertos.</li>
+                  <li> Aumentar la satisfacción personal de los empleados de la empresa.</li>
+                  <li> Crear una actitud para dar preferencia a la calidad por encima de la productividad.</li>
+                  <li> Detener los errores antes de que sucedan.</li>
+                  <li> Es un programa global de la empresa en el que cada persona y departamento asume que es cliente y proveedor.</li>
+                  <li> Progreso continuo hacia la obtención de cero defectos en los impresos, cero retrasos en las entregas, cero defectos en las oficinas, cero averías en las máquinas y cero stocks en los almacenes.</li>
+                </ul>
+              </span>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
