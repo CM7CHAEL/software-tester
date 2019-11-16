2b1a12865900806fcc1b478446749456f1979b50
Michael Cervera <michaelcervera13@gmail.com>
2019-11-15 23:43:12 -0500

    footer

--- src/components/Routes.js
+++ src/components/Routes.js
@@ -8,7 +8,7 @@ function Routes() {
     <div className="Proceso">
       <BrowserRouter>
         <Switch>
-          <Route path="/" component={Fundamentos} />
+          <Route exact path="/" component={Fundamentos} />
           <Route path="/proceso" component={ProcesoTesting} />
         </Switch>
       </BrowserRouter>
